3bc9e721cc7ee6a5036131edfaee58f86b508f8d
SantiagoAlejandro <santiago.moreno01@uptc.edu.co>
2022-12-13 20:34:44 -0500

    Configuracion

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyATL95Rmd_eOHgnxy35m_I5MMZt1rPefDQ",
+  authDomain: "backend-gym-dc9c1.firebaseapp.com",
+  projectId: "backend-gym-dc9c1",
+  storageBucket: "backend-gym-dc9c1.appspot.com",
+  messagingSenderId: "826776091928",
+  appId: "1:826776091928:web:699abc8399c33cb4a3b26d",
+  measurementId: "G-MFBLH02RTX"
+};
+
+// Initialize Firebase
+const analytics = getAnalytics(app);
+  module.exports={server}
 require('./database/db');
 const apiRouter = require('./routes/api/api_router')
 const routerHello = require("./routes/api/message")
 const cors = require('cors')
 
 const express = require("express")
-const  app = express(),
+const  app = express(firebaseConfig),
 bodyParser = require("body-parser"),
 methodOverride = require("method-override"),
 morgan = require("morgan");
@@ -26,5 +47,4 @@ app.use("/", routerHello)
 const server = app.listen(app.get('port'), function () {
    });
   console.log(`Node server running on http://localhost:${app.get('port')}`);
-
-  module.exports={server}
+// Import the functions you need from the SDKs you need
